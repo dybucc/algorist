660e3678540064c53ceef21657bdcddbfddfb66a
dybucc <149513579+dybucc@users.noreply.github.com>
2026-02-01 19:32:10 +0100

    chore: keep iterating on the design of the crate
    The `Fields` API should be fine now. The `FieldBuilder` API should also
    be fairly alright, so work has continue on implementing the full
    `GraphBackend` trait, and the implementation on the default backend.

--- algorist-grapht/src/lib.rs
+++ algorist-grapht/src/lib.rs
@@ -1,9 +1,9 @@
 #![allow(dead_code, reason = "The crate is a WIP.")]
 
-use algorist_grapht_macros::add;
 use std::{
     any::{Any, TypeId},
-    collections::{HashMap, hash_map::Entry},
+    collections::HashMap,
+    ops::{Deref, DerefMut},
 };
 
 #[derive(Debug)]
@@ -29,14 +29,21 @@ struct Graph<'a> {
     id: Option<usize>,
 }
 
-impl<'a> GraphBackend for Graph<'a> {
+impl<'a, 'b> GraphBackend<'b> for Graph<'a>
+where
+    'b: 'a,
+{
     type Vertex = Vertex<'a>;
     type Arc = Arc<'a>;
+    type Error = GraphError;
+
+    const ARC_ALLOCS: usize = 102;
+    const EXTRA_N: usize = 4;
 
     fn new(n: usize) -> Self {
         Graph {
             vertices: {
-                let mut output = Vec::with_capacity(n);
+                let mut output = Vec::with_capacity(n + Self::EXTRA_N);
                 output.resize_with(n, || Vertex { arcs: Vec::new() });
 
                 output
@@ -57,7 +64,30 @@ impl<'a> GraphBackend for Graph<'a> {
         self.m
     }
 
-    fn add_arc(&mut self) {}
+    fn new_arc<T>(&mut self, u: &'b T, v: &'b T) -> Result<(), Self::Error>
+    where
+        T: AsRef<Self::Vertex>,
+    {
+        self.arcs.reserve(Self::ARC_ALLOCS);
+        self.vertices
+            .last_mut()
+            .map(|elem| {
+                self.arcs.push(Arc { tip: v.as_ref() });
+                elem.arcs.push(self.arcs.last().unwrap());
+            })
+            .ok_or(GraphError(GraphErrorReason::NoVerticesInGraph))
+    }
+    fn new_edge<T>(&mut self, u: &T, v: &T)
+    where
+        T: AsRef<Self::Vertex>,
+    {
+    }
+}
+
+struct GraphError(GraphErrorReason);
+
+enum GraphErrorReason {
+    NoVerticesInGraph,
 }
 
 struct FieldBuilder(HashMap<TypeId, Vec<Box<dyn Any>>>);
@@ -69,46 +99,40 @@ impl FieldBuilder {
         Self(HashMap::new())
     }
 
-    fn add_field<T>(mut self, field: T) -> Self
+    #[doc(alias = "insert", alias = "add")]
+    fn touch<T>(mut self) -> Self
     where
-        for<'a> T: 'a + PartialEq,
+        for<'a> T: 'a + Default,
     {
-        // Can't chain all entry API methods because both of their closures move
-        // `field`.
-        match self.0.entry(field.type_id()) {
-            Entry::Vacant(_) => {
-                self.0
-                    .entry(field.type_id())
-                    .and_modify(|existing_fields| existing_fields.push(Box::new(field)));
-            }
-            Entry::Occupied(_) => {
-                self.0.entry(field.type_id()).or_insert_with(|| {
-                    let input: Box<dyn Any> = Box::new(field);
+        let ty_id = TypeId::of::<T>();
+        self.0
+            .entry(ty_id)
+            .and_modify(|existing_fields| existing_fields.push(Box::new(T::default())))
+            .or_insert_with(|| {
+                // Need separate declaration because the inference algorithm
+                // defaults to creating a `Box` of `T` and not a trait object.
+                let input: Box<dyn Any> = Box::new(T::default());
 
-                    vec![input]
-                });
-            }
-        }
+                vec![input]
+            });
 
         self
     }
 
     // The first field whose `PartialEq` trait implementation compares equal
     // will be the one removed.
-    fn rm_field<T>(&mut self, field: &T) -> Option<T>
+    #[doc(alias = "remove", alias = "del", alias = "delete")]
+    fn rm<T>(&mut self) -> Option<()>
     where
-        for<'a> T: 'a + PartialEq,
+        for<'a> T: 'a,
     {
-        match self.0.get_mut(&field.type_id()) {
-            Some(fields) => fields
-                .iter()
-                .position(|elem| elem.downcast_ref::<T>().unwrap() == field)
-                .map(|idx| *fields.swap_remove(idx).downcast::<T>().unwrap()),
-            None => None,
-        }
+        self.0.get_mut(&TypeId::of::<T>()).map(|fields| {
+            fields.pop();
+        })
     }
 
-    fn consume_fields_of<T>(&mut self) -> Option<FieldContainer<T>>
+    #[doc(alias = "consume")]
+    fn own<T>(&mut self) -> Option<FieldContainer<T>>
     where
         for<'a> T: 'a,
     {
@@ -116,7 +140,12 @@ impl FieldBuilder {
             FieldContainer(
                 entry
                     .into_iter()
-                    .map(|elem| *elem.downcast::<T>().unwrap())
+                    .map(|elem| {
+                        *elem.downcast::<T>().expect(
+                            "`elem` should safely downcast to `T` because it's extracted from the \
+                            `typeid` key of `T`.",
+                        )
+                    })
                     .collect(),
             )
         })
@@ -131,36 +160,58 @@ impl Default for FieldBuilder {
 
 struct FieldContainer<T>(Vec<T>);
 
-pub trait Field<T, const N: usize> {
+impl<T> Deref for FieldContainer<T> {
+    type Target = Vec<T>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl<T> DerefMut for FieldContainer<T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
+impl<T> AsRef<Vec<T>> for FieldContainer<T> {
+    fn as_ref(&self) -> &Vec<T> {
+        self.deref()
+    }
+}
+
+impl<T> AsMut<Vec<T>> for FieldContainer<T> {
+    fn as_mut(&mut self) -> &mut Vec<T> {
+        self.deref_mut()
+    }
+}
+
+trait Field<T, const N: usize> {
     fn get<'a>() -> &'a T;
     fn set(other: &T);
 }
 
 trait Fields<T, const N: usize> {}
 
-pub trait GraphBackend {
+trait GraphBackend<'a> {
     type Vertex;
     type Arc;
+    type Error;
+
+    const ARC_ALLOCS: usize;
+    const EXTRA_N: usize;
 
     fn new(n: usize) -> Self;
 
     fn n(&self) -> usize;
     fn m(&self) -> usize;
 
-    fn add_arc(&mut self);
-}
-
-impl<T> Fields<String, 2> for T where T: Field<String, 0> + Field<String, 1> {}
-
-#[expect(private_bounds)]
-#[cfg_attr(not(doc), add)]
-pub fn planar_graph<T>(_: &T)
-where
-    T: GraphBackend + Fields<String, 2>,
-    T::Vertex: Fields<u32, 1>,
-{
-    <T as Field<String, 0>>::get();
-    <T::Vertex as Field<u32, 0>>::get();
+    fn new_arc<T>(&mut self, u: &'a T, v: &'a T) -> Result<(), Self::Error>
+    where
+        T: AsRef<Self::Vertex>;
+    fn new_edge<T>(&mut self, u: &T, v: &T)
+    where
+        T: AsRef<Self::Vertex>;
 }
 
 #[cfg(test)]
@@ -173,7 +224,7 @@ mod tests {
     fn it_works() {
         // TODO: implement a macro that lets me access each field more
         // ergonomically inside of the function.
-        #[add]
+        #[cfg_attr(not(doc), add)]
         fn planar_graph<T>(_: &T)
         where
             T: GraphBackend + Fields<String, 2>,
